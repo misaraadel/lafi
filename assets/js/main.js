0252241f7722e132701b55e70f2624719b7174e1
misaraadel <misaraadel@gmail.com>
2022-03-27 15:20:10 +0200

    edit data

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,45 @@ $(document).ready(function () {
         });
     });
 
-    //fancybox
-    $("[data-fancybox]").fancybox({
-        selector: '[data-fancybox="images"]',
-        loop: true
+    $('.owl-products').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 20,
+        nav:false,
+        dots: true,
+        autoplay: false,
+        touchDrag  : true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:2
+            },
+            400: {
+                items: 2
+            },
+            500: {
+                items: 3
+            },
+            750:{
+                items:4
+            },
+            1000:{
+                items:4
+            }
+        }
     });
+});
+
+$("[data-fancybox]").fancybox({
+    selector: '[data-fancybox="images"]',
+    loop: true
 });
